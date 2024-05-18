0e931266474121fe1fd02d5b511af43e1b635c61
yudi.gunawan <yudi.gunaone87@gmail.com>
2024-05-18 11:59:42 +0700

    First Commit

---
+++ src/main/java/com/mycompany/javaoppv2/JavaOppv2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.javaoppv2;
+
+/**
+ *
+ * @author user
+ */
+public class JavaOppv2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
